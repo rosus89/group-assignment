eb10bb8a61024d6dcaf69ce989c2c33107606641
rosus89 <rosus89@gmail.com>
2022-11-28 11:50:12 +0000

    Bootstrap v1

---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+const initialState= {}
+
+// will probably use this later
+// State changing actions go here 
+// more info here https://reactjs.org/docs/hooks-reference.html#usereducer
+
+function reducer (state, action) {
+    switch (action.type) {
+        case 'a':
+            return {
+            }
+        case 'b':
+            return {
+            }
+        default:
+            return state;
+    
+    }
+}
+
+
+
+export {reducer, initialState};
