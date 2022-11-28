6729f7b4f944561e850ef6592ac182007b577c64
rosus89 <rosus89@gmail.com>
2022-11-28 18:18:01 +0000

    Style:layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,14 @@
 import Header from './Header'
 import Footer from './Footer'
+import './Layout.css'
 
 function Layout({children}) {
     return (
-        <>
+        <div className='wrapper' role="none">
         <Header />
         {children}
         <Footer />
-        </>
+        </div>
     )
 }
 
