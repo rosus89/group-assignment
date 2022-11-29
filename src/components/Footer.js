2cade3989a50fdff9179092a9ab8c68f9e7bbf2f
Shoregill <80356831+Shoregill@users.noreply.github.com>
2022-11-29 15:08:35 +0000

    Footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,15 @@
-function Footer() {
-    return <p>Footer</p>
-}
+import "./Footer.css"
 
+function Footer() {
+    return (
+        <footer>
+            <ul>
+            <li className='footer-link'><a  href="/">Like us on Facebook</a></li>
+            <li className='footer-link'><a  href="/about">Follow us on Twitter</a></li>
+            <li className='footer-link'><a  href="/products">Check us out on Instagram</a></li>
+        </ul>
+        </footer>
+    )
+}
 
 export default Footer
