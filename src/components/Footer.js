2b70ddd5158825494378ac6a65fd92b90f66fe4f
rosus89 <rosus89@gmail.com>
2022-11-29 18:15:28 +0000

    Fix: alt missing, minor errors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ function Footer() {
     return (
         <footer>
             <ul>
-            <li className='footer-link'><a  href="https://www.facebook.com/" target="_blank"><img src="Facebook_logo.png" width="25" height="25"/>  Like us on Facebook</a></li>
-            <li className='footer-link'><a  href="https://twitter.com/" target="_blank"><img src="Twitter-logo.png" width="25" height="25"/>  Follow us on Twitter</a></li>
-            <li className='footer-link'><a  href="https://www.instagram.com/" target="_blank"><img src="Instagram_icon.png" width="25" height="25"/>  Check us out on Instagram</a></li>
-            <li className='footer-link'><a  href="https://github.com/rosus89/group-assignment" target="_blank"><img src="GitHub-Mark-32px.png" width="25" height="25"/>  (C) 2022 Team A</a></li>
+            <li className='footer-link'><a  href="https://www.facebook.com/" ><img src="Facebook_logo.png" alt="facebook"/>  Like us on Facebook</a></li>
+            <li className='footer-link'><a  href="https://twitter.com/" ><img src="Twitter-logo.png" alt="twitter"/>  Follow us on Twitter</a></li>
+            <li className='footer-link'><a  href="https://www.instagram.com/" ><img src="Instagram_icon.png" alt="instagram"/>  Check us out on Instagram</a></li>
+            <li className='footer-link'><a  href="https://github.com/rosus89/group-assignment" ><img src="GitHub-Mark-32px.png" alt="Github"/>  (C) 2022 Team A</a></li>
         </ul>
         </footer>
     )
