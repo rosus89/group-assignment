42b2b29e1ff1a986de87e0ebca35e82821538c3b
rosus89 <rosus89@gmail.com>
2022-11-29 13:47:11 +0000

    Header Layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import NavBar from "./NavBar"
 import './Header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBasketShopping } from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
     return (
     <header>
         logo
         <NavBar/>
-        basket
+        <div className="basket">
+            <FontAwesomeIcon icon={ faBasketShopping }/>
+        </div>
     </header>
     )
 }
