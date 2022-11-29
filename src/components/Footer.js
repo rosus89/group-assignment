29ed717011a8132671d5f98af166360348a8af7f
Shoregill <80356831+Shoregill@users.noreply.github.com>
2022-11-29 15:53:38 +0000

    logos added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ function Footer() {
     return (
         <footer>
             <ul>
-            <li className='footer-link'><a  href="https://www.facebook.com/" target="_blank">Like us on Facebook</a></li>
-            <li className='footer-link'><a  href="https://twitter.com/" target="_blank">Follow us on Twitter</a></li>
-            <li className='footer-link'><a  href="https://www.instagram.com/" target="_blank">Check us out on Instagram</a></li>
-            <li className='footer-link'><a  href="https://github.com/rosus89/group-assignment" target="_blank">(C) 2022 Team A</a></li>
+            <li className='footer-link'><a  href="https://www.facebook.com/" target="_blank"><img src="Facebook_logo.png" width="25" height="25"/>Like us on Facebook</a></li>
+            <li className='footer-link'><a  href="https://twitter.com/" target="_blank"><img src="Twitter-logo.png" width="25" height="25"/>Follow us on Twitter</a></li>
+            <li className='footer-link'><a  href="https://www.instagram.com/" target="_blank"><img src="Instagram_icon.png" width="25" height="25"/>Check us out on Instagram</a></li>
+            <li className='footer-link'><a  href="https://github.com/rosus89/group-assignment" target="_blank"><img src="GitHub-Mark-32px.png" width="25" height="25"/>(C) 2022 Team A</a></li>
         </ul>
         </footer>
     )
