5fdf423a1ec5bc434b17337662602a047151e0d5
Shoregill <80356831+Shoregill@users.noreply.github.com>
2022-11-29 15:28:49 +0000

    Added social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,9 @@ function Footer() {
         <footer>
             <ul>
             <li className='footer-link'><a  href="/">Like us on Facebook</a></li>
-            <li className='footer-link'><a  href="/about">Follow us on Twitter</a></li>
-            <li className='footer-link'><a  href="/products">Check us out on Instagram</a></li>
+            <li className='footer-link'><a  href="https://twitter.com/" target="_blank">Follow us on Twitter</a></li>
+            <li className='footer-link'><a  href="https://www.instagram.com/" target="_blank">Check us out on Instagram</a></li>
+            <li className='footer-link'><a  href="https://github.com/rosus89/group-assignment" target="_blank">(C) 2022 Team A</a></li>
         </ul>
         </footer>
     )
