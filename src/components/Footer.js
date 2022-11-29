bb13e23fcd42156a42009d573424a8b09b419e6e
Shoregill <80356831+Shoregill@users.noreply.github.com>
2022-11-29 15:30:51 +0000

    Facebook added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer>
             <ul>
-            <li className='footer-link'><a  href="/">Like us on Facebook</a></li>
+            <li className='footer-link'><a  href="https://www.facebook.com/" target="_blank">Like us on Facebook</a></li>
             <li className='footer-link'><a  href="https://twitter.com/" target="_blank">Follow us on Twitter</a></li>
             <li className='footer-link'><a  href="https://www.instagram.com/" target="_blank">Check us out on Instagram</a></li>
             <li className='footer-link'><a  href="https://github.com/rosus89/group-assignment" target="_blank">(C) 2022 Team A</a></li>
