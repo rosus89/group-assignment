444790818f38c5236b35d306c44b4c6184032ab6
Shoregill <80356831+Shoregill@users.noreply.github.com>
2022-11-30 11:08:52 +0000

    Basket

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,43 @@ function reducer (state, action) {
             }
         case 'removeFromBasket':
             return {
+            }const initialState= {removeFrom:[]}
+
+            function reducer (state, action) {
+                switch (action.type) {
+                    case 'add':
+                        return {
+                            ...state,
+                            addTo: [...state.addTo, createAddTo(state,action.payload)]
+                        }
+                    case 'remove':
+                        return {
+                            ...state,
+                            removeFrom: state.removeFrom.filter(removeFrom => removeFrom.id !== action.payload)
+                        };
+                    case 'complete':
+                        return {
+                            ...state,
+                            complete: toggleComplete(state, action.payload)
+                        }
+                    default:
+                        return state;
+                
+                }
             }
+            
+            function addTo (state, value) {
+                return {
+                    id: state.addTo.length > 0 ? state.addTo[state.addTo.length-1].id + 1 : 1,
+                    value,
+                    complete: false }
+            }
+            
+            const toggleAddTo = (state, id) => state.addTo.map( addTo => addTo.id === id ? {...addTo, complete:!addTo.complete} : addTo)
+            
+            
+            
+            export {reducer, initialState};
         default:
             return state;
     
