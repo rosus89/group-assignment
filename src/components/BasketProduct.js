b21448f26337fcc597c96af5c3d0ed1ac9da3a5c
rosus89 <rosus89@gmail.com>
2022-12-05 14:10:24 +0000

    display amount in basket

--- src/components/BasketProduct.js
+++ src/components/BasketProduct.js
@@ -3,8 +3,8 @@ import './BasketProduct.css'
 function BasketProduct ({product, dispatch}) {
     return (
         <div className='product'>
-            <div className='product-details' ><h4>{product.name}</h4><p>{product.price}</p></div>
-            <div className='remove' onClick={()=>dispatch({type: 'removeFromBasket', payload:product.id})}>Remove</div>
+            <div className='product-details' ><h4>{product.name}</h4><p>{product.price}</p><p>{product.amount}</p></div>
+            <div className='remove' onClick={()=>dispatch({type: 'removeFromBasket', payload:product._id})}>Remove</div>
             
         </div>
     )
