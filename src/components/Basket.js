0707e5e146b7ed66aedb8ff6693d3defb18218a7
rosus89 <rosus89@gmail.com>
2022-12-05 14:10:12 +0000

    Display and calculate total

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -3,10 +3,13 @@ import "./Basket.css"
 import BasketProduct from "./BasketProduct"
 
 function Basket({basket, dispatch,display}) {
+    const total = (basket)=>basket.reduce((sum, product)=>sum+parseInt(product.price.replace('£','')),0)
+    console.log(total)
     return (
        <div className= {`modal ${display ? "show" : "hidden"}`}>
             <div className="modal-content">
-            {basket.map(product => <BasketProduct key={product.id} product={product} dispatch={dispatch}></BasketProduct>)}
+            {basket.map(product => <BasketProduct key={product._id} product={product} dispatch={dispatch}></BasketProduct>)}
+            <p>Total: £{total(basket)}</p>
             </div>
 
        </div>
